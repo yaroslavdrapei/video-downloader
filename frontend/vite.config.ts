a99c57536fa9296f51d61ff075512467a0009e87
yaroslav.drapei <yaroslav.drapei@signus.ai>
2025-08-25 19:52:10 +0300

    feat(frontend): change port back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
